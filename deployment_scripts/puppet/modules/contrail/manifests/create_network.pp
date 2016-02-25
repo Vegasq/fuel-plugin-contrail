eab66451b4cb20681a5ee68a1bf77ca9b5a74172
Mykola Yakovliev <my5878@att.com>
2016-02-25 09:38:02 -0600

    Removed code that crates contrail networks

--- deployment_scripts/puppet/modules/contrail/manifests/create_network.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/create_network.pp
@@ -4,31 +4,32 @@ define contrail::create_network (
   $tenant_name   = 'admin',
   )
 {
-
-  if $netdata['L3']['floating'] {
-    $alloc = split($netdata['L3']['floating'], ':')
-    $allocation_pools = "start=${alloc[0]},end=${alloc[1]}"
-  }
-
-  notify {"${name} ::: router_ext ${netdata['L2']['router_ext']}":}
-  notify {"${name} ::: tenant ${netdata['tenant']}":}
-  notify {"${name} ::: shared ${netdata['shared']}":}
-
-  neutron_network { $name:
-    ensure          => present,
-    router_external => $netdata['L2']['router_ext'],
-    tenant_name     => $tenant_name,
-    shared          => $netdata['shared']
-  }
-
-  neutron_subnet { "${name}__subnet":
-    ensure           => present,
-    cidr             => $netdata['L3']['subnet'],
-    network_name     => $name,
-    tenant_name      => $tenant_name,
-    gateway_ip       => $netdata['L3']['gateway'],
-    enable_dhcp      => $netdata['L3']['enable_dhcp'],
-    dns_nameservers  => $netdata['L3']['nameservers'],
-    allocation_pools => $allocation_pools,
-  }
+#  So far this code executed on every node. But for controller-only
+#  deployment it's kinda problem.
+#  if $netdata['L3']['floating'] {
+#    $alloc = split($netdata['L3']['floating'], ':')
+#    $allocation_pools = "start=${alloc[0]},end=${alloc[1]}"
+#  }
+#
+#  notify {"${name} ::: router_ext ${netdata['L2']['router_ext']}":}
+#  notify {"${name} ::: tenant ${netdata['tenant']}":}
+#  notify {"${name} ::: shared ${netdata['shared']}":}
+#
+#  neutron_network { $name:
+#    ensure          => present,
+#    router_external => $netdata['L2']['router_ext'],
+#    tenant_name     => $tenant_name,
+#    shared          => $netdata['shared']
+#  }
+#
+#  neutron_subnet { "${name}__subnet":
+#    ensure           => present,
+#    cidr             => $netdata['L3']['subnet'],
+#    network_name     => $name,
+#    tenant_name      => $tenant_name,
+#    gateway_ip       => $netdata['L3']['gateway'],
+#    enable_dhcp      => $netdata['L3']['enable_dhcp'],
+#    dns_nameservers  => $netdata['L3']['nameservers'],
+#    allocation_pools => $allocation_pools,
+#  }
 }
